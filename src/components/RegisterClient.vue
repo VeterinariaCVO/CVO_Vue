db1f07f08ae6555980f6e6782280c357392e0900
joan <oob071947@gmail.com>
2026-03-05 23:28:36 -0600

    correccion en component

--- src/components/RegisterClient.vue
+++ src/components/RegisterClient.vue
@@ -1,26 +1,86 @@
 <template>
 <div class="max-w-md mx-auto mt-10 p-6 bg-white shadow rounded">
 
-<h2 class="text-2xl font-bold mb-4">Registrar Cliente</h2>
+<h2 class="text-xl font-bold mb-4">Registrar Cliente</h2>
 
 <form @submit.prevent="registerUser">
 
-<input v-model="user.name" type="text" placeholder="Nombre"
-class="w-full border p-2 mb-3 rounded"/>
+<input v-model="user.name" placeholder="Nombre"
+class="border p-2 w-full mb-3"/>
 
-<input v-model="user.email" type="email" placeholder="Email"
-class="w-full border p-2 mb-3 rounded"/>
+<input v-model="user.email" placeholder="Email"
+class="border p-2 w-full mb-3"/>
 
-<input v-model="user.phone" type="text" placeholder="Teléfono"
-class="w-full border p-2 mb-3 rounded"/>
+<input v-model="user.phone" placeholder="Teléfono"
+class="border p-2 w-full mb-3"/>
 
-<input v-model="user.address" type="text" placeholder="Domicilio"
-class="w-full border p-2 mb-3 rounded"/>
+<input v-model="user.address" placeholder="Domicilio"
+class="border p-2 w-full mb-3"/>
 
 <input v-model="user.password" type="password" placeholder="Password"
-class="w-full border p-2 mb-3 rounded"/>
+class="border p-2 w-full mb-3"/>
 
-<button
-class="bg-blue-500 text-white px-4 py-2 rounded w-full">
+<button class="bg-blue-500 text-white p-2 w-full">
 Registrar
 </button>
+
+</form>
+
+</div>
+</template>
+
+<script>
+   export default {
+
+    data(){
+    return{
+        user:{
+        name:"",
+        email:"",
+        phone:"",
+        address:"",
+        password:"",
+        role_id:1,
+        active:true
+        }
+    }
+},
+
+methods:{
+
+async registerUser(){
+
+try{
+
+const response = await fetch("http://127.0.0.1:8000/api/users",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body: JSON.stringify(this.user)
+
+})
+
+const data = await response.json()
+
+alert("Cliente registrado correctamente")
+
+console.log(data)
+
+}catch(error){
+
+console.error(error)
+
+alert("Error al registrar")
+
+}
+
+}
+
+}
+
+}
+</script>
