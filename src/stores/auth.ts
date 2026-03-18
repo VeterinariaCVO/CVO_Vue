860f7109d5d30fb01c9381081e19f8a7b619977d
Natalia Diaz <nataliadiazalvarado2705@gmail.com>
2026-03-18 08:19:05 -0600

    loginparte 1

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,22 +1,30 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
 
+interface User {
+  id: number
+  name: string
+  email: string
+  role: number
+}
+
 export const useAuthStore = defineStore('auth', () => {
-  const token = ref<string | null>(localStorage.getItem('token'))
-  const user  = ref<any | null>(null)
+
+  const token = ref<string | null>(null)
+  const user = ref<User | null>(null)
 
   function setToken(newToken: string) {
     token.value = newToken
     localStorage.setItem('token', newToken)
   }
 
-  function setUser(newUser: any) {
+  function setUser(newUser: User) {
     user.value = newUser
   }
 
   function clear() {
     token.value = null
-    user.value  = null
+    user.value = null
     localStorage.removeItem('token')
   }
 
