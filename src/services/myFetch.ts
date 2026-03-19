1296274e1797d7d563b59b1919b25def73cb470b
joan <oob071947@gmail.com>
2026-03-19 17:54:00 -0600

    primer commit

--- src/services/myFetch.ts
+++ src/services/myFetch.ts
@@ -14,16 +14,41 @@ export const pato = createFetch({
   },
 
   options: {
-    immediate: false,
-    updateDataOnError: true,
-    async beforeFetch({}) {},
-    afterFetch(ctx) {
-      return ctx
-    },
-    onFetchError(ctx) {
-      ctx.error = new Error('hubo un error')
-      return ctx
-    },
+  // Interceptor: Se ejecuta justo antes de cada petición
+  async beforeFetch({ options, cancel }) {
+    const authStore = useAuthStore()
+    if (!authStore.token){
+      cancel()
+    }
+
+    if (authStore.token) {
+      // Garantizamos que headers sea un objeto y añadimos el Bearer
+      options.headers = {
+        ...options.headers,
+        Authorization: `Bearer ${authStore.token}`,
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      }
+    }
+
+    return { options }
   },
 
+  // Interceptor: Manejo de errores global (ej. Token expirado)
+  onFetchError(ctx) {
+    const authStore = useAuthStore()
+
+    if (ctx.response?.status === 401) {
+      authStore.logout()
+      router.push('/login')
+      // Opcional: redirigir al login con
+    }
+
+    return ctx
+  },
+},
+// Opciones de fetch nativo (opcional)
+fetchOptions: {
+  mode: 'cors',
+},
 })
