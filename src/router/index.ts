bd75c4c384e9ad0dfde617190a65ced640cc24b4
Mateo Martinez <aslan19070620@gmail.com>
2026-03-20 18:49:10 -0600

    separacion por carpetas

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,18 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/authStore'
 
 // Views
-import HomeView from '@/views/HomeView.vue'
-import LoginViewTest from '@/views/LoginViewTest.vue'
+import LoginViewTest from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-      meta: { requiresAuth: true },
-    },
     {
       path: '/login',
       name: 'login',
