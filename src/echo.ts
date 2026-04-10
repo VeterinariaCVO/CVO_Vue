cb805bdb5adf1ceb07f91353ea2955ec135811f6
Natalia Diaz <nataliadiazalvarado2705@gmail.com>
2026-04-10 01:42:13 -0600

    echo para serv

--- src/echo.ts
+++ src/echo.ts
@@ -6,12 +6,16 @@ window.Pusher = Pusher
 const echo = new Echo({
   broadcaster: 'reverb',
   key: import.meta.env.VITE_REVERB_APP_KEY,
+
   wsHost: import.meta.env.VITE_REVERB_HOST,
-  wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-  wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-  forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
+  wsPort: 443,
+  wssPort: 443,
+
+  forceTLS: true,
   enabledTransports: ['ws', 'wss'],
+
   authEndpoint: `${import.meta.env.VITE_API_URL}/broadcasting/auth`,
+
   auth: {
     headers: {
       Authorization: `Bearer ${localStorage.getItem('token')}`,
